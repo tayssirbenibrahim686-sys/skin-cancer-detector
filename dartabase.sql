ad3948b63d391cfda0af57ef16fca7ba00ef89cc
Tayssir <tayssirbenibrahim686@gmail.com>
2026-05-31 11:00:58 +0100

    Update database schema

--- dartabase.sql
+++ dartabase.sql
@@ -1,8 +1,17 @@
-CREATE DATABASE skin_cancer_db;
+ CREATE DATABASE skin_cancer_db;
 USE skin_cancer_db;
 
-CREATE TABLE users(
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50)
-    password
+    username VARCHAR(50),
+    password VARCHAR(100)
+);
+
+CREATE TABLE patients (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    age INT,
+    result VARCHAR(50),
+    probability FLOAT,
+    image_path VARCHAR(255)
 )
